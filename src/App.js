7418997f4e72180911726d35897f0eaf2f760172
Biswajit Biswas <jfbiswajit@gmail.com>
2022-05-10 21:45:07 +0600

    Add: S15E10

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       },
       transformTasks
     );
-  }, []);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
